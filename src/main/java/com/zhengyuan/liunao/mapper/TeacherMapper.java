847778375c7005fda3bc2376e63f6363c3824f9b
lwc6 <923300204@qq.com>
2022-12-06 16:12:02 +0800

    diyi

---
+++ src/main/java/com/zhengyuan/liunao/mapper/TeacherMapper.java
@@ -0,0 +1,27 @@
+package com.zhengyuan.liunao.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.zhengyuan.liunao.entity.Teacher;
+
+@Mapper
+public interface TeacherMapper {
+	public List<Teacher> findTeacher(String teachno, String psw);
+	public int addTeacher(Map map);
+
+	public int deleteByForeachT(List<String> teachno);
+
+	public int deleteTeacher(String teachno);
+
+	public List<Teacher> findAllTeacher(Map<String, Object> map);
+
+	public List<Teacher> findTeacherByName(String name, int start, int pagesize);
+
+	public int teacherCount();
+	
+	public List<Teacher> findTeacherByNum(String num);
+
+	public int updateTea(Map map);
+}
